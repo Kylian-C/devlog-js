750c161cd4e25b125c1d71073f2cae1aa60f45a7
Kylian <Kylian.crohem@le-campus-numerique.fr>
2023-06-01 14:23:24 +0200

    exo 5 complet

--- exo-5/script-5.js
+++ exo-5/script-5.js
@@ -1,3 +1,16 @@
 // you can write js here
 
 console.log('exo-5');
+
+var input = "Bonjour"
+var voyelles = ["a","e","i","o","u","y"]
+var resultArray = []
+
+for (var i=0 ; i < voyelles.length ; i++){
+    for (var j=0; j < voyelles.length ; j++){
+        if (input [i] == voyelles[j]){
+            resultArray.push(voyelles[j])
+        }
+    }
+}
+console.log (resultArray.join(" ").toUpperCase())
